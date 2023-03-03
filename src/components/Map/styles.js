6530a29284942c0ecbcc2d2c5fa2b0d318c044ca
Keenah <kvancamp@mail.com>
2023-03-03 16:47:36 -0600

    Update styles.js

--- src/components/Map/styles.js
+++ src/components/Map/styles.js
@@ -1,7 +1,24 @@
-import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
 
-const Map = () => {
-  return <h1>Map</h1>;
-};
-
-export default Map;
+export default makeStyles(() => ({
+  paper: {
+    padding: "10px",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    width: "100px",
+  },
+  mapContainer: {
+    height: "85vh",
+    width: "100%",
+  },
+  markerContainer: {
+    position: "absolute",
+    transform: "translate(-50%, -50%)",
+    zIndex: 1,
+    "&:hover": { zIndex: 2 },
+  },
+  pointer: {
+    cursor: "pointer",
+  },
+}));
