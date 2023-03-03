864a3f0fa4daffe4455823a4dfdba5a1bfad9519
Keenah <kvancamp@mail.com>
2023-03-03 12:39:11 -0600

    added imports

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,4 +1,8 @@
 import React from "react";
+import GoogleMapReact from "google-map-react";
+import { Paper, Typography, useMediaQuery } from "@material-ui/core";
+import LocationCityOutlinedIcon from "@material-ui/icons/LocationOutlined";
+import Rating from "@material-ui/lab";
 
 const Map = () => {
   return <h1>Map</h1>;
