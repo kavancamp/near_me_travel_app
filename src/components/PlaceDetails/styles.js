93fc1898ce19a619053b4af03d4d2a88a0aefccf
Keenah <kvancamp@mail.com>
2023-03-07 09:44:01 -0600

    Update styles.js

--- src/components/PlaceDetails/styles.js
+++ src/components/PlaceDetails/styles.js
@@ -1,7 +1,18 @@
-import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
 
-const PlaceDetails = () => {
-  return <h1>Place Details</h1>;
-};
-
-export default PlaceDetails;
+export default makeStyles(() => ({
+  chip: {
+    margin: "5px 5px 5px 0",
+  },
+  subtitle: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "space-between",
+    marginTop: "10px",
+  },
+  spacing: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+}));
